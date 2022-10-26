b73891d8fedbfff9fa6070de14f4f5bda51b7f2a
Cristian Vera <c.vera@ensolvers.com>
2022-10-25 22:45:52 -0500

    fixed

--- cleaning-page/src/App.js
+++ cleaning-page/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { Route,Routes } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import Navbar from './components/features/Navbar/Navbar';
+import AboutSection from './components/features/AboutSection/AboutSection';
+import ServicesSection from "./components/features/ServicesSection/ServicesSection"
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <Routes>
-          <Route path={"/"} element={<Home/>}/>
-      </Routes>
+      <Home/>
+      <ServicesSection/>
+      <AboutSection/>
     </div>
   );
 }
